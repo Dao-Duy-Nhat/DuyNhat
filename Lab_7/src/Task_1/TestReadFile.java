bf8399484a0274a53facd322924da76b285ceb20
Dao-Duy-Nhat <daoduynhat12a2@gmail.com>
2023-11-02 22:03:28 +0700

    CTDL

---
+++ Lab_7/src/Task_1/TestReadFile.java
@@ -0,0 +1,18 @@
+package Task_1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class TestReadFile {
+	public static void main(String[] args) throws FileNotFoundException {
+		// Scanner input = new Scanner(new File("data/hamlet.txt"));
+		Scanner input = new Scanner(new File("fit.txt"));
+
+		while (input.hasNext()) {
+			String word = input.next();
+			System.out.println(word);
+		}
+	}
+}
